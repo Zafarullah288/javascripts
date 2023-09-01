3fa92902b8cf790081a70957863931b99613b791
zafarullah <139501409+Zafarullah288@users.noreply.github.com>
2023-09-01 22:59:36 +0500

    Add files via upload
    assignment javascripts

---
+++ app.js
@@ -0,0 +1,46 @@
+// !!chapter: 1 !!
+// "Answer 1"
+alert("zafarullah")
+
+// "Answer 2"
+alert("Error!please valid a number.")
+
+// "Answer 3"
+alert("Welcome to Js..\nHappy coding!")
+
+// "Answer 4"
+alert("Welcome to JavaScript")
+aler("Happy coding!\n▢prevent this page from creating additional dialog")
+
+// "Answer 5"
+let text;
+let text2 = prompt("Please enter your message:");
+alert(text2) 
+
+// !!chapter: 2 !!
+// "Answer 1 and 2" 
+var name="zafarullah"
+var name2="jamali"
+console.log( name + name2)
+
+// "Answer 3" 
+alert("salam")
+
+// "Answer 4"
+alert("zafarullah")
+alert("age : 20")
+alert("welcome to javascript")
+
+// "Answer 5"
+alert("pizza \nburger \nsandwish \nfries \nb")
+    
+// "Answer 6"
+var email="zafarjamali2002@gmail.com"
+alert("my email address is "+email)
+
+// "Answer 7"
+var book="I Am Trying To Lern From Smarter Way To Learn JS"
+alert(book)
+
+// "Answer 8"  
+document.writeln("my name is zafarullah")
